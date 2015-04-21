e5142619e9ab9124300eaf4bf3173d460cd119ad
derekdomo <xiangyus@andrew.cmu.edu>
2015-04-21 01:54:31 -0400

    parameter v7

--- src/CacheDatabase.java
+++ src/CacheDatabase.java
@@ -1,28 +1,30 @@
-import java.io.*;
-import java.util.HashMap;
+import java.io.Serializable;
 import java.rmi.RemoteException;
+import java.util.HashMap;
+
 class CacheDatabase implements Cloud.DatabaseOps, Serializable {
-	public HashMap<String, String> cache = new HashMap<String, String>();
-	public Cloud.DatabaseOps db; 
-	public CacheDatabase(Cloud.DatabaseOps db) {
-		this.db = db;
-	}
-	public String get(String key) throws RemoteException {
-		if (cache.containsKey(key)) {
-			return cache.get(key);	
-		} else {
-			String ret = db.get(key);
-			cache.put(key, ret);
-			return ret;
-		}
-	}
-	public boolean set(String key, String val, String auth) throws RemoteException {
-		if (cache.containsKey(key)) {
-			cache.put(key, val);
-		}
-		return db.set(key, val, auth);
-	}
-	public boolean transaction(String item, float price, int qty) throws RemoteException {
-		return db.transaction(item, price, qty);
-	}
+    public HashMap<String, String> cache = new HashMap<String, String>();
+    public Cloud.DatabaseOps db;
+
+    public CacheDatabase(Cloud.DatabaseOps db) {
+        this.db = db;
+    }
+
+    public String get(String key) throws RemoteException {
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        } else {
+            String ret = db.get(key);
+            cache.put(key, ret);
+            return ret;
+        }
+    }
+
+    public boolean set(String key, String val, String auth) throws RemoteException {
+        return db.set(key, val, auth);
+    }
+
+    public boolean transaction(String item, float price, int qty) throws RemoteException {
+        return db.transaction(item, price, qty);
+    }
 }
